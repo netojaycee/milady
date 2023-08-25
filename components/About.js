093ae884eb5e2141973523f29b686841a8ad9936
Edeh JAYCEE <netojaycee@gmail.com>
2023-08-25 02:44:55 -0700

    f

--- components/About.js
+++ components/About.js
@@ -5,26 +5,26 @@ class About extends Component {
         return (
             <div className='text-success pt-5 px-4'>
           <h1 className='text-center'>About $OMF OnlyMiladyFans Token</h1>
-          <p className=''>
+          <p>
 
 In a realm where tokens weave connections tight,
 A community blossoms, bathed in digital light.
 Unified hearts, bound by a shared decree,
-Embracing the vision of what's yet to be.
+Embracing the vision of what&apos;s yet to be.
 
 Hand in hand, creators and collectors align,
 A symphony of dreams, where stars intertwine.
 Each MiladyOnlyFans token, a shimmering thread,
 Bridging the gap between minds widespread.
 
-In this realm of pixels and blockchain's grace,
+In this realm of pixels and blockchain&apos;s grace,
 The significance blooms in every embrace.
-For within this community's resplendent sphere,
+For within this community&apos;s resplendent sphere,
 Lies a future astronomical, boldly clear.
 
 Tokens traded not just for gains alone,
 But for the stories and artistry sown.
-Creators' visions, collectors' treasures untold,
+Creators&apos; visions, collectors&apos; treasures untold,
 The value of presence, more precious than gold.
 
 As we journey together, bound by this fate,
@@ -32,9 +32,9 @@ The future unfolds, a celestial gate.
 Astronomical heights our passions shall scale,
 As MiladyOnlyFans sets sail on the gale.
 
-So let's cherish the ties that we tightly weave,
+So let&apos;s cherish the ties that we tightly weave,
 In this realm where the daring believe.
-For the token's potential is vast and bright,
+For the token&apos;s potential is vast and bright,
 A future that shines like a guiding light.
 
 Disclaimer</p>
